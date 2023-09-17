ea09810aa022ea483ec30889b995de19c19ca562
khedhiri adel <khedhiri.adel@gmail.com>
2023-09-17 09:32:58 +0100

    2commit

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/<int:x>/<int:y>')
 @app.route('/<int:x>/<int:y>/<color1>/<color2>')
 def checkerboard(x=8, y=8, color1='red', color2='black'):
-    return render_template('checkerboard.html', x=x, y=y, color1=color1, color2=color2)
+    return render_template('index.html', x=x, y=y, color1=color1, color2=color2)
 
 if __name__ == '__main__': 
     app.run(debug=True)
